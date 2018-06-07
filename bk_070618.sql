390672f8254ca5ab4696c7960496e34fc1c847de
Crijosicar <crijosicar@hotmail.com>
2018-06-07 01:05:17 -0500

    Update bk_070618.sql

--- bk_070618.sql
+++ bk_070618.sql
@@ -1,8 +1,12 @@
-CREATE SCHEMA `prueba` ;
+#CREATE SCHEMA `prueba` ;
 
-CREATE TABLE `prueba`.`prueba` (
-  `idprueba` INT NOT NULL,
-  `pruebacol` VARCHAR(45) NULL,
-  `pruebacol1` VARCHAR(45) NULL,
-  `pruebacol2` VARCHAR(45) NULL,
-  PRIMARY KEY (`idprueba`));
+#CREATE TABLE `prueba`.`prueba` (
+#  `idprueba` INT NOT NULL,
+#  `pruebacol` VARCHAR(45) NULL,
+#  `pruebacol1` VARCHAR(45) NULL,
+#  `pruebacol2` VARCHAR(45) NULL,
+#  PRIMARY KEY (`idprueba`));
+
+CREATE USER docker;
+CREATE DATABASE docker;
+GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
